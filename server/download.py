be83af6e8d834d901c3fbba5110f4afbcacf8d05
Ahmed Ibrahim <ib153507@gmail.com>
2021-11-14 22:19:15 +0200

    Throw an error if the file doesn't exist

--- server/download.py
+++ server/download.py
@@ -6,5 +6,8 @@ bp = Blueprint('download', __name__, url_prefix='/download')
 
 @bp.route('/<string:model>/<string:filename>')
 def download(model, filename):
+    file_path = os.path.join(current_app.root_path, '..', model, filename)
+    if not os.path.exists(file_path):
+        return jsonify({'error': f"`{model}/{filename}` doesn't exist"})
     return send_file(os.path.join(current_app.root_path, '..', model, filename))
     
