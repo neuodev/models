5cc167f721b444a555f2bbdc6e2105030b18ed43
Ahmed Ibrahim <ib153507@gmail.com>
2021-11-14 22:07:23 +0200

    Create a small server to run download the model

---
+++ server/download.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, current_app, send_file
+import os
+from flask.json import jsonify 
+
+bp = Blueprint('download', __name__, url_prefix='/download')
+
+@bp.route('/<string:model>/<string:filename>')
+def download(model, filename):
+    return send_file(os.path.join(current_app.root_path, model, filename))
+    
