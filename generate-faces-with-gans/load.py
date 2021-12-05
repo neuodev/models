ca609d2f8eb061e2c28528daf758ad31fd7dbcdf
Ahmed Ibrahim <ib153507@gmail.com>
2021-12-05 15:01:44 +0200

    Save sample image

--- generate-faces-with-gans/load.py
+++ generate-faces-with-gans/load.py
@@ -10,7 +10,7 @@ _, axes = plt.subplots(3, 3, figsize=(20, 20))
 axes = axes.flatten()
 
 i = 0
-for example in ds:
+for example in ds['train']:
     axes[i].imshow(example['image'], cmap='gray_r')
     axes[i].set_title(example['label'])
     axes[i].axis('off')
