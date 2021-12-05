1f3a74c3b45caff038ca7ce84daa5cd393456d1a
Ahmed Ibrahim <ib153507@gmail.com>
2021-12-05 14:30:04 +0200

    Create new project

---
+++ generate-faces-with-gans/load.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt 
+
+datasets = tfds.list_builders()
+ds = tfds.load('celeb_a', split='train', shuffle_files=True)
+print(ds)
+
+_, axes = plt.subplots(3, 3, figsize=(20, 20))
+axes = axes.flatten()
+for i in range(len(axes)):
+    axes[i].imshow(ds['image'][i])
+    axes[i].axis('off')
+
+plt.savefig('sample.png')
